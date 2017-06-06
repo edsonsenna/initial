24d6240c80a240fb7fc177609834f81a2a400571
Edson de Senna Junior <edsonsjr14@outlook.com>
2017-06-06 14:13:01 -0300

    Testanto git

---
+++ initial/src/initial/Interface.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package initial;
+
+/**
+ *
+ * @author Edson
+ */
+public class Interface {
+    public static void main(String[] args) {
+        System.out.println("Teste");
+    }
+}
